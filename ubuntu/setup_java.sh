22ab3b055935ce9ab19d264ecef6fda31dd15d62
vekatveeramanidaasan <ve_krish@yahoo.com>
2015-03-18 02:41:05 -0700

    Setting up sun java on ubuntu box

---
+++ ubuntu/setup_java.sh
@@ -0,0 +1,30 @@
+if [ $# -eq 0 ]
+  then
+    echo "No arguments supplied"
+    exit 1
+fi
+if [ -z "$1" ]
+  then
+    echo "Pass the expected JDK folder name"
+    exit 1
+fi
+
+JAVA_DIR=$1
+sudo apt-get purge openjdk-\*
+sudo mkdir -p /usr/local/java
+cd ~/Downloads
+sudo cp -r jdk*.tar.gz /usr/local/java
+cd /usr/local/java
+sudo tar xvzf jdk*.tar.gz
+echo "JAVA_HOME=/usr/local/java/$JAVA_DIR" | sudo tee -a /etc/profile
+echo 'PATH=$PATH:$JAVA_HOME/bin:' | sudo tee -a /etc/profile
+echo 'export JAVA_HOME' | sudo tee -a /etc/profile
+echo 'export PATH' | sudo tee -a /etc/profile
+sudo update-alternatives --install "/usr/bin/java" "java" "/usr/local/java/$JAVA_DIR/bin/java" 1
+sudo update-alternatives --install "/usr/bin/javac" "javac" "/usr/local/java/$JAVA_DIR/bin/javac" 1
+sudo update-alternatives --install "/usr/bin/javaws" "javaws" "/usr/local/java/$JAVA_DIR/bin/javaws" 1
+sudo update-alternatives --set "/usr/bin/java" "java" "/usr/local/java/$JAVA_DIR/bin/java" 1 
+sudo update-alternatives --set "/usr/bin/javac" "javac" "/usr/local/java/$JAVA_DIR/bin/javac" 1
+sudo update-alternatives --set "/usr/bin/javaws" "javaws" "/usr/local/java/$JAVA_DIR/bin/javaws" 1
+. /etc/profile
+java -version
